21da26d3c086eb47d1d40e401cb95f5aa3b2cafe
efty20 <rakibulhasan52020@gmail.com>
2026-06-09 09:11:30 +0600

    Add project files.

---
+++ BookGridWeb/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookGridWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+    }
+}
---
+++ BookGridWeb/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BookGridWeb.Models
+{
+    public class Category
+    {
+        public int  Id { get; set; }
+        public string Name { get; set; } = string.Empty;    
+    }
+}
