9468c289656da62a28296eba4ad8fba5da9bd496
efty20 <rakibulhasan52020@gmail.com>
2026-06-16 02:22:20 +0600

    adding the"add new categories option"

---
+++ BookGridWeb/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using BookGridWeb.Data;
+using BookGridWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookGridWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            var categories = _context.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Create")]
+        public IActionResult CreatePOST(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BookGridWeb/Models/Category.cs
+++ BookGridWeb/Models/Category.cs
@@ -3,6 +3,7 @@
     public class Category
     {
         public int  Id { get; set; }
-        public string Name { get; set; } = string.Empty;    
+        public string Name { get; set; } = string.Empty;
+        public int  DisplayOrder { get; set; }
     }
 }
